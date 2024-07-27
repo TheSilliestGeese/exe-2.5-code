14add14880b2a5d79a05e52fa1a50c7a58e8c76c
TheSilliestGeese <126857288+TheSilliestGeese@users.noreply.github.com>
2024-07-27 01:48:51 +0100

    Update RetroFilter.hx

--- source/RetroFilter.hx
+++ source/RetroFilter.hx
@@ -14,126 +14,83 @@ import openfl.Lib;
 class RetroFilter extends FlxShader // https://www.shadertoy.com/view/WdffW2
 {
     @:glFragmentSource('
- // Automatically converted with https://github.com/TheLeerName/ShadertoyToFlixel
+        #pragma header
 
-#pragma header
+        uniform float iTime;
+        uniform vec2 iResolution;
+        uniform bool vignetteOn;
+        uniform bool perspectiveOn;
+        uniform bool distortionOn;
+        uniform bool scanlinesOn;
+        uniform bool vignetteMoving;
+        uniform sampler2D noiseTex;
+        uniform float glitchModifier;
 
-#define round(a) floor(a + 0.5)
-#define iResolution vec3(openfl_TextureSize, 0.)
-uniform float iTime;
-#define iChannel0 bitmap
-uniform sampler2D iChannel1;
-uniform sampler2D iChannel2;
-uniform sampler2D iChannel3;
-#define texture flixel_texture2D
 
-// third argument fix
-vec4 flixel_texture2D(sampler2D bitmap, vec2 coord, float bias) {
-	vec4 color = texture2D(bitmap, coord, bias);
-	if (!hasTransform)
-	{
-		return color;
-	}
-	if (color.a == 0.0)
-	{
-		return vec4(0.0, 0.0, 0.0, 0.0);
-	}
-	if (!hasColorTransform)
-	{
-		return color * openfl_Alphav;
-	}
-	color = vec4(color.rgb / color.a, color.a);
-	mat4 colorMultiplier = mat4(0);
-	colorMultiplier[0][0] = openfl_ColorMultiplierv.x;
-	colorMultiplier[1][1] = openfl_ColorMultiplierv.y;
-	colorMultiplier[2][2] = openfl_ColorMultiplierv.z;
-	colorMultiplier[3][3] = openfl_ColorMultiplierv.w;
-	color = clamp(openfl_ColorOffsetv + (color * colorMultiplier), 0.0, 1.0);
-	if (color.a > 0.0)
-	{
-		return vec4(color.rgb * color.a * openfl_Alphav, color.a * openfl_Alphav);
-	}
-	return vec4(0.0, 0.0, 0.0, 0.0);
-}
+        float hash11(float a)
+        {
+            return fract(53.156*sin(a*45.45))-.5;
+        }
+        float dispnoise(float a)
+        {
+            float a1 = hash11(floor(a)),a2=hash11(ceil(a));
+            return .03*mix(a1,a2,pow(fract(a),8.));
+        }
+        float noise(float a)
+        {
+            float a1 = hash11(floor(a)),a2=hash11(ceil(a));
+            return mix(a1+.5,a2+.5,pow(fract(a),50.));
+        }
+        
+        float hash21(vec2 a)
+        {
+            return fract(sin(dot(a,vec2(12.9898,78.233))+iTime)*43758.5453);
+        }
+        float perlin(vec2 a)
+        {
+            a*=vec2(100.,500.);
+            float a1 = hash21(floor(a));
+            float a2 = hash21(floor(a)+vec2(1,0));
+            float a3 = hash21(floor(a)+vec2(0,1));
+            float a4 = hash21(ceil(a));
+            return pow(mix(mix(a1,a2,fract(a.x)),mix(a3,a4,fract(a.x)),fract(a.y)),2.);  
+        }
+        
+        
+        vec4 grade(vec4 color)
+        {
+            color = pow(color,vec4(2.2));
+            color*= vec4(1.3,.7,.89,1);
+            
+            color = pow(color,vec4(.4));
+            return 1.3*color;
+        }
+        
+        
+        void main()
+        {
+            vec2 fragCoord = openfl_TextureCoordv * iResolution;
 
-// variables which is empty, they need just to avoid crashing shader
-uniform float iTimeDelta;
-uniform float iFrameRate;
-uniform int iFrame;
-#define iChannelTime float[4](iTime, 0., 0., 0.)
-#define iChannelResolution vec3[4](iResolution, vec3(0.), vec3(0.), vec3(0.))
-uniform vec4 iMouse;
-uniform vec4 iDate;
-
-/* License CC BY-NC-SA 4.0 Deed */
-/* https://creativecommons.org/licenses/by-nc-sa/4.0/ */
-
-float noise(vec2 p)
-{
-	float s = texture(iChannel1,vec2(1.,2.*cos(iTime))*iTime*8. + p*1.).x;
-	s *= s;
-	return s;
-}
-
-float onOff(float a, float b, float c)
-{
-	return step(c, sin(iTime + a*cos(iTime*b)));
-}
-
-float ramp(float y, float start, float end)
-{
-	float inside = step(start,y) - step(end,y);
-	float fact = (y-start)/(end-start)*inside;
-	return (1.-fact) * inside;
-	
-}
-
-float stripes(vec2 uv)
-{
-	
-	float noi = noise(uv*vec2(0.5,1.) + vec2(1.,3.));
-	return ramp(mod(uv.y*4. + iTime/2.+sin(iTime + sin(iTime*0.63)),1.),0.5,0.6)*noi;
-}
-
-vec3 getVideo(vec2 uv)
-{
-	vec2 look = uv;
-	float window = 1./(1.+20.*(look.y-mod(iTime/4.,1.))*(look.y-mod(iTime/4.,1.)));
-	look.x = look.x + sin(look.y*10. + iTime)/50.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window;
-	float vShift = 0.4*onOff(2.,3.,.9)*(sin(iTime)*sin(iTime*20.) + 
-										 (0.5 + 0.1*sin(iTime*200.)*cos(iTime)));
-	look.y = mod(look.y + vShift, 1.);
-	vec3 video = vec3(texture(iChannel0,look));
-	return video;
-}
-
-vec2 screenDistort(vec2 uv)
-{
-	uv -= vec2(.5,.5);
-	uv = uv*1.2*(1./1.2+2.*uv.x*uv.x*uv.y*uv.y);
-	uv += vec2(.5,.5);
-	return uv;
-}
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = fragCoord.xy / iResolution.xy;
-	uv = screenDistort(uv);
-	vec3 video = getVideo(uv);
-	float vigAmt = 3.+.3*sin(iTime + 5.*cos(iTime*5.));
-	float vignette = (1.-vigAmt*(uv.y-.5)*(uv.y-.5))*(1.-vigAmt*(uv.x-.5)*(uv.x-.5));
-	
-	video += stripes(uv);
-	video += noise(uv*2.)/2.;
-	video *= vignette;
-	video *= (12.+mod(uv.y*30.+iTime,1.))/13.;
-	
-	fragColor = vec4(video,texture(iChannel0, uv).a);
-}
-
-void main() {
-	mainImage(gl_FragColor, openfl_TextureCoordv*openfl_TextureSize);
-}')
+            vec2 uv = fragCoord.xy / iResolution.xy;
+            float disp = dispnoise(.7*uv.y+mod(iTime,200.)*.2);
+            uv.x+=disp;
+                if(hash11(floor(iTime*4.)/4.)>.47)
+                uv.y+=.5*hash11(floor(iTime*8.)/4.);
+            uv =fract(uv);
+            
+            vec4 color  = texture2D(bitmap ,uv);
+            color = grade(color);
+            if(hash11(uv.y+floor((iTime+uv.y)*16.)/16.)>.497)
+                color+=hash11(floor(100.*(uv.x-iTime)))+.5;
+            color = mix(color,vec4(.3),max(0.,sin(uv.y*20.)*perlin(.3*uv)*.5));
+            
+            gl_FragColor = color;
+            if(abs(2.*gl_FragCoord.y-iResolution.y)>iResolution.y-50.*noise(5.*iTime+(uv.y>.5?0.:10.)))
+                gl_FragColor=vec4(perlin(uv)+.5);
+            if(abs(2.*gl_FragCoord.x-(1.-disp)*iResolution.x)>(4./3.)*iResolution.y)
+                gl_FragColor=vec4(0);
+        }
+    ')
     public function new()
     {
         super();
